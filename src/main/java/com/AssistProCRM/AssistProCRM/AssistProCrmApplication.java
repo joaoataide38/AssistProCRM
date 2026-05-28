dc7f80e0a463be8d56e0bb0d66ff02006e9a28fa
joaoataide38 <jmendesataide@gmail.com>
2026-05-27 23:50:08 -0300

    initial commit

---
+++ src/main/java/com/AssistProCRM/AssistProCRM/AssistProCrmApplication.java
@@ -0,0 +1,16 @@
+package com.AssistProCRM.AssistProCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistProCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistProCrmApplication.class, args);
+	pong();
+	}
+	static void pong(){
+	System.out.println("pong");
+}
+}
---
+++ src/test/java/com/AssistProCRM/AssistProCRM/AssistProCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AssistProCRM.AssistProCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistProCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
